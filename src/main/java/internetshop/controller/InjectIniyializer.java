799111b28d21165bf339694085e9fcb37c47d33f
Korfat <38156885+Korfat@users.noreply.github.com>
2019-09-19 19:19:10 +0300

    Create logging

--- src/main/java/internetshop/controller/InjectIniyializer.java
+++ src/main/java/internetshop/controller/InjectIniyializer.java
@@ -5,14 +5,18 @@ import internetshop.lib.Injector;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.apache.log4j.Logger;
+
 public class InjectIniyializer implements ServletContextListener {
+    private static final Logger logger = Logger.getLogger(InjectIniyializer.class);
+
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         try {
-            System.out.println("Dependency injection started ...");
+            logger.info("Dependency injection started ...");
             Injector.injectDependency();
         } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
+            logger.fatal("FAILED: ", e);
         }
     }
 
